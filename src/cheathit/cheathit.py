6716571bf03579d5259a59915bc39cd43fcde384
Danylo Mysak <danmysak@gmail.com>
2022-01-25 16:40:38 +0200

    UI text improved

--- src/cheathit/cheathit.py
+++ src/cheathit/cheathit.py
@@ -5,10 +5,10 @@ import sys
 from .analyzer import analyze
 from .shared import GROUP, STUDENT, PROBLEM, ATTEMPT
 
-ALLOWED_COMPONENTS = [GROUP, STUDENT, PROBLEM, ATTEMPT]
+ALLOWED_COMPONENTS = [STUDENT, GROUP, PROBLEM, ATTEMPT]
 COMPONENT_DELIMITER = '/'
 DEFAULT_PATH = STUDENT
-COMPONENT_DESCRIPTION = f'{", ".join(map(repr, ALLOWED_COMPONENTS))}'f' delimited with {repr(COMPONENT_DELIMITER)}'
+COMPONENT_DESCRIPTION = f'{", ".join(map(repr, ALLOWED_COMPONENTS))}'f' separated with {repr(COMPONENT_DELIMITER)}'
 
 DEFAULT_MIN_NGRAM = 1
 DEFAULT_MAX_NGRAM = 20
@@ -26,9 +26,9 @@ SUBMISSION_MATCH_DELIMITER = '+'
 def parse_path(path: str) -> tuple[str, ...]:
     components = path.split(COMPONENT_DELIMITER)
     if not all(component in ALLOWED_COMPONENTS for component in components):
-        raise ValueError(f'Expected {COMPONENT_DESCRIPTION}, got {repr(path)}')
+        raise argparse.ArgumentTypeError(f'Expected {COMPONENT_DESCRIPTION}, got {repr(path)}')
     if STUDENT not in components:
-        raise ValueError(f'Expected {repr(STUDENT)} among the path components, got {repr(path)}')
+        raise argparse.ArgumentTypeError(f'Expected {repr(STUDENT)} among the path components, got {repr(path)}')
     return tuple(components)
 
 
@@ -38,7 +38,7 @@ def parse_positive_integer(value: str) -> int:
     except ValueError:
         size = None
     if size is None or size <= 0:
-        raise ValueError(f'Expected positive integer, got {value}')
+        raise argparse.ArgumentTypeError(f'Expected positive integer, got {repr(value)}')
     return size
 
 
@@ -48,7 +48,7 @@ def parse_ratio(value: str) -> float:
     except ValueError:
         ratio = None
     if ratio is None or not 0.0 <= ratio <= 1.0:
-        raise ValueError(f'Expected a number between 0.0 and 1.0, got {value}')
+        raise argparse.ArgumentTypeError(f'Expected a number between 0.0 and 1.0, got {repr(value)}')
     return ratio
 
 
@@ -62,18 +62,18 @@ def log(*args, **kwargs) -> None:
 
 def run() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('root', type=Path, help='Root directory with submissions')
+    parser.add_argument('root', type=Path, help='directory with submissions')
     parser.add_argument('--path', type=parse_path, nargs='?', default=DEFAULT_PATH,
-                        help=f'Template for the path to individual submissions: {COMPONENT_DESCRIPTION}')
+                        help=f'template for the path to individual submissions: {COMPONENT_DESCRIPTION}')
     parser.add_argument('--min-ngram', type=parse_positive_integer, nargs='?', default=DEFAULT_MIN_NGRAM,
-                        help='Minimum size of ngrams to analyze')
+                        help='minimum size of ngrams to analyze')
     parser.add_argument('--max-ngram', type=parse_positive_integer, nargs='?', default=DEFAULT_MAX_NGRAM,
-                        help='Maximum size of ngrams to analyze')
+                        help='maximum size of ngrams to analyze')
     parser.add_argument('--min-ratio', type=parse_ratio, nargs='?', default=DEFAULT_MIN_RATIO,
-                        help='Minimum ratio of the number of shared tokens'
-                             ' to the number of tokens in the longer submission')
+                        help='minimum ratio of the number of shared tokens'
+                             ' to the number of tokens in the longer of the two submissions')
     parser.add_argument('--max-clique', type=parse_positive_integer, nargs='?', default=None,
-                        help=f'If an ngram occurs in submissions of more than so many students (or in submissions'
+                        help=f'if an ngram occurs in submissions of more than so many students (or in submissions'
                              f' of students from more than so many groups), it is excluded from analysis')
     arguments = parser.parse_args()
     root: Path = arguments.root
@@ -92,6 +92,12 @@ def run() -> None:
                        else (DEFAULT_CLIQUE_GROUP if has_groups else DEFAULT_CLIQUE_INDIVIDUAL))
     if max_clique == 1 and not has_groups:
         parser.error('Max clique size must be greater than 1 if students are not assigned groups')
+    log(f'Running with '
+        f'path={COMPONENT_DELIMITER.join(path)}, '
+        f'min-ngram={min_ngram}, '
+        f'max-ngram={max_ngram}, '
+        f'min-ratio={min_ratio}, '
+        f'max-clique={max_clique}')
     analysis = analyze(root, path, min_ngram, max_ngram, min_ratio, max_clique, log)
     print(', '.join(filter(None, [
         numeral(analysis.submission_count, 'submission'),
@@ -110,5 +116,5 @@ def run() -> None:
             print(submission_match.a)
             print(submission_match.b)
             print(numeral(submission_match.common_tokens, 'common token')
-                  + ' / ' + numeral(len(submission_match.longest_ngram), 'token') + ' in the longest common match: ')
+                  + ' / ' + numeral(len(submission_match.longest_ngram), 'token') + ' in the longest match:')
             print(' '.join(submission_match.longest_ngram))
